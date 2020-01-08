0d6dceccd08ab7d5efc55caea84f075324850cd0
mharb <mohamed.harb@cegedim.com>
2020-01-08 14:21:46 +0200

    Added bootstrap

--- help-desk-ui/src/main/web/src/app/app.component.spec.ts
+++ help-desk-ui/src/main/web/src/app/app.component.spec.ts
@@ -19,17 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'help-desk-ui'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('help-desk-ui');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('help-desk-ui app is running!');
-  });
 });
