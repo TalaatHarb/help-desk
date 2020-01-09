abdd06b4c50476bd49ce54c8e2d92209be38936b
mharb <mohamed.harb@cegedim.com>
2020-01-09 12:20:19 +0200

    Configured swagger to only check the api package

--- help-desk-api/src/main/java/net/talaatharb/helpdesk/config/SwaggerConfiguration.java
+++ help-desk-api/src/main/java/net/talaatharb/helpdesk/config/SwaggerConfiguration.java
@@ -26,7 +26,8 @@ public class SwaggerConfiguration {
 	 */
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any()).build();
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("net.talaatharb.helpdesk.api")).paths(PathSelectors.any())
+				.build();
 	}
 }
